8091e6e7f4a6e5f87c2bdd262c01301082b5977f
Felipe <felipeandres_uc@outlook.com>
2019-04-02 00:07:10 -0300

    tercer commit

--- emprendedor2.rb
+++ emprendedor2.rb
@@ -1,20 +1,20 @@
-usuarios=ARGV
-precio=ARGV
-premium=ARGV
-gratuitos=ARGV
-gastos=ARGV
+usuarios=ARGV[0]
+precio=ARGV[1]
+preciopremium=ARGV[2]
+gratuitos=ARGV[3]
+gastos=ARGV[4]
 
 
-utilidades=(ARGV[0].to_f * (ARGV[1].to_f + (ARGV[2].to_f + ARGV[2].to_f ) )) - ARGV[4].to_f
+utilidades=(usuarios.to_f * (precio.to_f + ( precio.to_f * 2.to_f ) )) - gastos.to_f
 
 
-if utilidades>=0
+if utilidades>0
 
     puts utilidades * 0.65
 
 
 else
 
-   puts utilidades
+   puts "0"
 
 end

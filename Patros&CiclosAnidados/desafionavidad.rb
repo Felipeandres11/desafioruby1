e00660fdd481e0ed0af016a634242de0cc99c136
Felipe <felipeandres_uc@outlook.com>
2019-04-12 16:55:54 -0400

    add

--- Patros&CiclosAnidados/desafionavidad.rb
+++ Patros&CiclosAnidados/desafionavidad.rb
@@ -1,32 +1,38 @@
 n = ARGV[0].to_i
 
-for i in (1..n)
-  for j in (1..n)
-    if (j*2)-n==i || (j-1)*2+i==n
-      print "*"
-    else
+def metodo_navidad(n)
+
+  for i in (1..n)
+    for j in (1..n)
+      if (j*2)-n==i || (j-1)*2+i==n
+        print "*"
+      else
+        print " "
+      end
+    end
+      print "\n"
+  end
+
+  for i in (1..n)
+    for j in (1..n)
+      if j==(n/2)+1
+        print "*"
+      else
+        print " "
+      end
+    end
+      print "\n"
+  end
+
+  for i in (1..n)
+    if i==1 || i==n
       print " "
+    else
+      print "*"
     end
   end
-    print "\n"
+  print " \n"
+
 end
 
-for i in (1..n)
-  for j in (1..n)
-    if j==(n/2)+1
-      print "*"
-    else
-      print " "
-    end
-  end
-    print "\n"
-end
-
-for i in (1..n)
-  if i==1 || i==n
-    print " "
-  else
-    print "*"
-  end
-end
-print " \n"
+metodo(n)
